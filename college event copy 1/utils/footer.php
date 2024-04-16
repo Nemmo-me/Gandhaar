f6c2a2a902ada8fe041b20365e78f3cf6df91bb9
Nirmiti Pardhe <77378590+Nemmo-me@users.noreply.github.com>
2024-04-16 12:47:35 +0530

    Add files via upload

---
+++ college event copy 1/utils/footer.php
@@ -0,0 +1,6 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <h6 style="text-align:center;"> Cummins College Of Engineering For Women, Pune</h6>
+    </div>
+</footer>
